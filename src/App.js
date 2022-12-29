3115794bc7647b4283c0ace69805bee5034b9524
hateriam <anohov_1983@mail.ru>
2022-12-29 19:12:49 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
 function App() {
+  const store = [{val: '1'}, {val: '2'}, {val: '3'}, {val: '4'}, {val: '5'}, {val: '+'}, {val: '6'}, {val: '7'}, {val: '8'},
+   {val: '9'}, {val: '0'}, {val: '-'}, {val: '*'}, {val: '/'}, {val: '.'}]
+
+  const operation = [{val: '='}, {val: 'C'}, {val: 'CE'}] 
+  const [value, setValue] = useState('0')
+
+  let buttons = store.map((button, index) => {
+    return <button key={index} onClick={() => typeNum(button.val)} >
+      {button.val}
+    </button>
+  })
+  let operations = operation.map((operat, index) => {
+    return <button key={index} onClick={() => doAct(operat.val)} >
+      {operat.val}
+    </button>
+  })
+
+  function typeNum (num) {
+    let currentVal = value
+    if (value === '0') {
+      currentVal = ''
+    } 
+    currentVal += num
+    setValue(currentVal)
+  }
+
+  function doAct(key) {
+    if (key === 'C') {
+      setValue('0')
+    } else if (key === 'CE') {
+      (value.length != '1') ? setValue(value.substring(0, value.length-1)) : setValue('0') 
+    } else if (key === '=') {
+      setValue(eval(value))
+      }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator">
+      <input value={value} type="text" />
+      {buttons}
+      {operations}
     </div>
   );
 }
